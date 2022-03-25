85888a3a41bb00ef6c4662831dbd166a516c5844
GodwillFoka <fokagodwill@gmail.com>
2022-03-25 08:25:42 +0100

    Mise à jour partie Administration

--- resources/views/admin/produit/show.blade.php
+++ resources/views/admin/produit/show.blade.php
@@ -1,11 +1,10 @@
-@extends('layouts.app')
+@extends('dashboard')
 
 @section('content')
     <div class="container">
         <div class="row">
-            @include('admin.sidebar')
 
-            <div class="col-md-9">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Produit {{ $produit->id }}</div>
                     <div class="card-body">
