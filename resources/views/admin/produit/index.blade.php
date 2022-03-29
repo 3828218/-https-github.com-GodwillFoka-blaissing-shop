7889248e67313c17540f52e0dfd7e3448195dd10
GodwillFoka <fokagodwill@gmail.com>
2022-03-29 02:48:08 +0200

    Correction de l'interface admin total

--- resources/views/admin/produit/index.blade.php
+++ resources/views/admin/produit/index.blade.php
@@ -48,7 +48,7 @@
                             <td class="text-center">
                                 <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-xl">
                                     <img class="taille2" style="border-radius: 80px; border: solid"
-                                        src="{{ asset('$item->image') }}" alt="{{ $item->nom }}" width="50px" height="50px"
+                                        src="{{ asset($item->image) }}" alt="{{ $item->nom }}" width="50px" height="50px"
                                         data-toggle="modal" data-target="#exampleModal" id="myImg" />
                                 </button>
                             <td>
